da0df757d9fe324152fc2653b91ce650105d874d
Pavel Potupalo <levapnero@gmail.com>
2025-06-22 13:27:40 +0300

    User entity implemented

--- src/main/java/org/model/User.java
+++ src/main/java/org/model/User.java
@@ -1,4 +1,74 @@
 package org.model;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private int id;
+    private int age;
+    private String name;
+    private String email;
+    private LocalDateTime created_at;
+
+    public User() {
+
+    }
+
+    public User(int id, int age, String name, String email, LocalDateTime created_at) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+        this.created_at = created_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", created_at=" + created_at +
+                '}';
+    }
 }
