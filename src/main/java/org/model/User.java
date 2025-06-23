4993e096ded675848092edd8a137976e5e8c4361
Pavel Potupalo <levapnero@gmail.com>
2025-06-23 03:42:10 +0300

    User linked to DB HibernateUtil implemented

--- src/main/java/org/model/User.java
+++ src/main/java/org/model/User.java
@@ -1,12 +1,23 @@
 package org.model;
 
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "age", nullable = false)
     private int age;
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
+    @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime created_at;
 
     public User() {
