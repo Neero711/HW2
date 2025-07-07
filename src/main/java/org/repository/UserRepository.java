26c860940b219f969bafe3c15c686a7e98c02ea0
Pavel Potupalo <levapnero@gmail.com>
2025-07-08 01:39:38 +0300

    Repository layer implementation

---
+++ src/main/java/org/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.repository;
+
+import org.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+}
