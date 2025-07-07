57ad73f78bc3664a6a33000953b1540af266192a
Pavel Potupalo <levapnero@gmail.com>
2025-07-08 01:34:51 +0300

    DTO implementation

--- src/main/java/org/dto/UserDTO.java
+++ src/main/java/org/dto/UserDTO.java
@@ -1,4 +1,17 @@
 package org.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDTO {
+    private Long id;
+    private Integer age;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
 }
--- src/main/java/org/dto/UserRequestDTO.java
+++ src/main/java/org/dto/UserRequestDTO.java
@@ -1,4 +1,24 @@
 package org.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDTO {
+    // C валидацией относительно реалистичных данных
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotNull(message = "Age is required")
+    @Min(value = 1, message = "Age should be greater than 0")
+    @Max(value = 100, message = "Age should be less than 100")
+    private Integer age;
 }
