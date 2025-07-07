5d3805fb64235e86f6ef9dd61a68b46025e2bd7e
Pavel Potupalo <levapnero@gmail.com>
2025-07-08 01:27:02 +0300

    User.java redone

--- src/main/java/org/model/User.java
+++ src/main/java/org/model/User.java
@@ -1,19 +1,25 @@
 package org.model;
 
-import java.time.LocalDateTime;
 import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
 import lombok.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "age", nullable = false)
-    private int age;
+    private Integer age;
     @Column(name = "name", nullable = false, length = 100)
     private String name;
     @Column(name = "email", nullable = false, unique = true, length = 100)
@@ -21,67 +27,4 @@ public class User {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime created_at;
-
-    public User() {
-
-    }
-
-    public User(Long id, int age, String name, String email, LocalDateTime created_at) {
-        this.id = id;
-        this.age = age;
-        this.name = name;
-        this.email = email;
-        this.created_at = created_at;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(LocalDateTime created_at) {
-        this.created_at = created_at;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", age=" + age +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", created_at=" + created_at +
-                '}';
-    }
 }
