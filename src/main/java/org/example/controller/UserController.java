0dbf0f596b7b17b2ac2d29bbe714525fce1a2cc0
Pavel Potupalo <levapnero@gmail.com>
2025-07-21 17:55:48 +0300

    Swagger HATEOAS impl getallusr imp

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,30 +1,50 @@
 package org.example.controller;
 
-import ch.qos.logback.core.model.Model;
 import org.example.dto.UserDTO;
 import org.example.dto.UserRequestDTO;
 import org.example.service.UserService;
 import jakarta.validation.Valid;
 import lombok.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.constraints.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "Управление пользователями", description = "API  по управлению пользователями")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Get all users", description = "Возвращает список пользователей")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list")
     @GetMapping
-    public List<UserDTO> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<CollectionModel<EntityModel<UserDTO>>> getAllUsers() {
+        List<EntityModel<UserDTO>> userModels = userService.getAllUsers().stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).getUserById(user.getId())).withSelfRel(),
+                        linkTo(UserController.class).slash(user.getId()).withRel("user"),
+                        linkTo(UserController.class).withRel("users")))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(CollectionModel.of(userModels,
+                linkTo(methodOn(UserController.class).getAllUsers()).withSelfRel()));
     }
+
 
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable Long id) {
