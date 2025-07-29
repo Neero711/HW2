3ca485eef89f6be5bf600b8da267957361948a4f
Pavel Potupalo <levapnero@gmail.com>
2025-07-29 03:17:30 +0300

    --

--- src/main/java/org/example/UserServiceApplication.java
+++ src/main/java/org/example/UserServiceApplication.java
@@ -3,7 +3,8 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.example")
+@SpringBootApplication
+
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
