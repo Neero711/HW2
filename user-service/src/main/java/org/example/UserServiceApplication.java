f115b55a22e697e13c8a3b368e2419fa77ecf940
Pavel Potupalo <levapnero@gmail.com>
2025-07-29 04:11:07 +0300

    --

--- user-service/src/main/java/org/example/UserServiceApplication.java
+++ user-service/src/main/java/org/example/UserServiceApplication.java
@@ -2,9 +2,9 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- user-service/src/test/java/org/example/controller/UserControllerIntegrationTest.java
+++ user-service/src/test/java/org/example/controller/UserControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.example.controller;
+package java.org.example.controller;
 
 import org.example.dto.UserDTO;
 import org.example.dto.UserRequestDTO;
